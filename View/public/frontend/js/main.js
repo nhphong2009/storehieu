a8e48edc445e6476dee98e2e3f22a09d814094c6
Phong Nguyen Huu <phongnh@ecommage.com>
2019-10-15 17:46:30 +0700

    Fix quantity on product detail page

--- View/public/frontend/js/main.js
+++ View/public/frontend/js/main.js
@@ -98,5 +98,35 @@
 	if (zoomMainProduct) {
 		$('#product-main-img .product-preview').zoom();
 	}
+	// Input number
+	$('.input-number').each(function() {
+		var $this = $(this),
+			$input = $this.find('input[type="number"]'),
+			up = $this.find('.qty-up'),
+			down = $this.find('.qty-down');
 
+		down.on('click', function () {
+			var value = parseInt($input.val()) - 1;
+			value = value < 1 ? 1 : value;
+			$input.val(value);
+			$input.change();
+			updatePriceSlider($this , value)
+		})
+
+		up.on('click', function () {
+			var value = parseInt($input.val()) + 1;
+			$input.val(value);
+			$input.change();
+			updatePriceSlider($this , value)
+		})
+	});
+	function updatePriceSlider(elem , value) {
+		if ( elem.hasClass('price-min') ) {
+			console.log('min')
+			priceSlider.noUiSlider.set([value, null]);
+		} else if ( elem.hasClass('price-max')) {
+			console.log('max')
+			priceSlider.noUiSlider.set([null, value]);
+		}
+	}
 })(jQuery);
